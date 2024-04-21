44987c8260e554c7c874ab3b76d5d69ba41f0206
Yanina Guadalupe <yaniferreira82@gmail.com>
2024-04-20 20:09:40 -0400

    juego funcionando

--- script.js
+++ script.js
@@ -4,34 +4,53 @@ window.addEventListener('load', init)
 const button = document.getElementById("guess-button");
 const input = document.getElementById("guess-input");
 const valor = input.value;
-function init(){
+function init() {
     console.log('Esto se ejecuta solo cuando se carga la pagina web')
 }
-function intentar(){
+function intentar() {
     const INTENTO = leerIntento();
-    if (INTENTO === palabra ) {
-        console.log("GANASTE!")
+    if (INTENTO === palabra) {
+        terminar("<h1>GANASTE!😀</h1>")
         return
     }
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
     for (let i in palabra){
-        if (INTENTO[i]===palabra[i]){
-            console.log(INTENTO[i], "VERDE")
-        } else if( palabra.includes(INTENTO[i]) ) {
-            console.log(INTENTO[i], "AMARILLO")
-        } else {
-            console.log(INTENTO[i], "GRIS")
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#79b851';
+        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#f3c237';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#a4aec4';
         }
+        ROW.appendChild(SPAN)
     }
-		intentos--
-    if (intentos==0){
-        console.log("PERDISTE!")
+    GRID.appendChild(ROW)
+
+    intentos--
+    if (intentos == 0) {
+        terminar("<h1>PERDISTE!😖</h1>")
     }
 }
 button.addEventListener("click", intentar);
-function leerIntento(){
+function leerIntento() {
     let intento = document.getElementById("guess-input");
     intento = intento.value;
-    intento = intento.toUpperCase(); 
+    intento = intento.toUpperCase();
     return intento;
 }
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    BOTON.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
 
